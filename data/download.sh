7688f996a3d977415b94577c487c48d1cedcfef8
Naoki Otani <notani@cs.cmu.edu>
2019-09-03 21:51:43 -0400

    Add a script to download datasets

---
+++ data/download.sh
@@ -0,0 +1,7 @@
+wget https://www.nyu.edu/projects/bowman/multinli/multinli_1.0.zip
+unzip multinli_1.0.zip
+rm -r __MACOSX/
+
+wget https://nlp.stanford.edu/projects/snli/snli_1.0.zip
+unzip snli_1.0.zip
+rm -r __MACOSX/
